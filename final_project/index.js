cea6878335de2055058d4c43a45f520676a3f4e2
ekopylova <jenya.kopylov@gmail.com>
2025-12-19 04:31:58 -0500

    progress

--- final_project/index.js
+++ final_project/index.js
@@ -8,13 +8,31 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true})
+)
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (!req.session || !req.session.authorization) {
+        return res.status(401).json({ message: "User not logged in!" });
+    }
+    const token = req.session.authorization['accessToken'];
+    if (!token){
+        return res.status(403).json({message: "User not logged in!"});
+    }
+    // Verify JWT token
+    jwt.verify(token, "access", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "User not authenticated" });
+        }
+        req.user = user;
+        next();   
+    });
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
